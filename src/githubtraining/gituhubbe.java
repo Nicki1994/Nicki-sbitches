fcf978e4ce2266ea6fde4ab3be15ba40db672c13
Mathias <mathias_1337@hotmail.com>
2017-11-13 15:00:10 +0100

    SovietWomble

--- src/githubtraining/gituhubbe.java
+++ src/githubtraining/gituhubbe.java
@@ -3,7 +3,7 @@ package githubtraining;
 public class gituhubbe {
 public static void main (String[] args) {
 	System.out.println("Stiv fisse!");
-	System.out.println("Fager was here, faggots");
+	System.out.println("Fager was here, fwaggots");
 }
 	// Fucking hell man kan det her
 }
