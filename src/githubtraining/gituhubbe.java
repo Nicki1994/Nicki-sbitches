c098e6a32e46d2435e49687e3a41b387c0268e34
Mathias <mathias_1337@hotmail.com>
2017-11-13 14:59:01 +0100

    I got dis shit boiiiiiiiiiiii

--- src/githubtraining/gituhubbe.java
+++ src/githubtraining/gituhubbe.java
@@ -3,6 +3,7 @@ package githubtraining;
 public class gituhubbe {
 public static void main (String[] args) {
 	System.out.println("Stiv fisse!");
+	System.out.println("Fager was here, faggots");
 }
 	// Fucking hell man kan det her
 }
