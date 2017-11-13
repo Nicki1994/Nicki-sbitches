a7e2d42145b4b3b572f4ccb49e6ca608b8ae880e
The-Hest <s175195@win.dtu.dk>
2017-11-13 14:56:35 +0100

    911

--- src/githubtraining/gituhubbe.java
+++ src/githubtraining/gituhubbe.java
@@ -1,8 +1,8 @@
 package githubtraining;
 
 public class gituhubbe {
-public static void main (String[] args) {
-	System.out.println("Stiv fisse!");
-}
+	public static void main (String[] args) {
+		System.out.println("Stiv fisse!");
+	}
 	// Fucking hell man kan det her
 }
